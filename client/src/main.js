26752afefaf1b31de369cb46e9b58ad7e8c28a6f
Gaziz1989 <gzhaiyk@gmail.com>
2018-05-23 18:06:56 +0600

    changing ports

--- client/src/main.js
+++ client/src/main.js
@@ -8,9 +8,9 @@ import router from './router'
 import Auth from '@/utils/Auth'
 import VModal from 'vue-js-modal'
 import Video from 'twilio-video'
-
+// https://chats-backend.mars.studio:8081/
 Vue.use(BootstrapVue)
-Vue.use(VueSocketio, 'http://138.68.103.82:8081?token=' + `${Auth().getToken()}`)
+Vue.use(VueSocketio, 'https://chats-backend.mars.studio:8081/?token=' + `${Auth().getToken()}`)
 Vue.prototype.$auth = new Auth()
 Vue.prototype.$video = Video
 Vue.use(VModal, { dynamic: true })
--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import Auth from '../utils/Auth'
-
+// https://chats-backend.mars.studio:8081/
 export default () => {
   return axios.create({
-    baseURL: `http://138.68.103.82:8081/`,
+    baseURL: `https://chats-backend.mars.studio:8081/`,
     headers: {
       Authorization: `Bearer ${Auth().getToken()}`
     }
