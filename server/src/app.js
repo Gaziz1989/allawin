744fa34567380d25cb6123dbae4727da8083398a
Gaziz1989 <gzhaiyk@gmail.com>
2018-07-26 13:44:42 +0600

    AAAA

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ const io = socket()
 io.use( async (socket, next) => {
   try {
     let access_token = socket.handshake.query.token
-
+    console.log(access_token, 17)
     const query = await db.query("SELECT * FROM \"user\" WHERE access_token = $1", [access_token])
     let user = query.rows[0]
     if (!user) {
